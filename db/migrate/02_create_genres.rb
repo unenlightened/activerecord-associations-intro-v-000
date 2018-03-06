1fd2c698cc6b9520b5076f4ef40800bc91bbdad8
Chely Ho <adept_inept@yahoo.com>
2018-03-06 00:08:00 +0000

    automatically backed up by learn

--- db/migrate/02_create_genres.rb
+++ db/migrate/02_create_genres.rb
@@ -1,6 +1,7 @@
 class CreateGenres < ActiveRecord::Migration[4.2]
   def change
-   create_table :artists do |t|
-     t.string :name
-   end
+    create_table :genres do |t|
+      t.string :name
+    end
+  end
 end
