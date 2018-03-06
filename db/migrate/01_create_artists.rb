31d41ad6addf260d489c3c48f0f58aae6f77967f
Chely Ho <adept_inept@yahoo.com>
2018-03-06 00:08:11 +0000

    automatically backed up by learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -1,9 +1,7 @@
 class CreateArtists < ActiveRecord::Migration[4.2]
   def change
-   create_table :songs do |t|
-     t.string :name
-     t.integer :artist_id
-     t.integer :genre_id
-   end
- end
+    create_table :artists do |t|
+      t.string :name
+    end
+  end
 end
